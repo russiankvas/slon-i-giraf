a6730009fa55bc8e10e2f4c15fe5fcb2d01ea37f
russiankvas <59476082+russiankvas@users.noreply.github.com>
2020-01-04 16:35:20 +0400

    Add files via upload

---
+++ lessen-3/main.js
@@ -0,0 +1,38 @@
+﻿function main () {
+  console.log(average([2,57,4]));
+}
+
+function average(arr) {
+  let m = 0;
+  for (let i = 0; i < arr.length; ++i) {
+    m += arr[i];
+  }
+	
+  return (m / arr.length);
+}
+
+function example() {
+    arr = [12, 13, 2.5 ,5]
+
+   console.log('числа в масиве ' + arr)
+   console.log('чисел в масиве ' + arr.length)
+
+   console.log('первое число в массиве ' + arr[0])
+   arr[2] = [1, 2, 3]
+   console.log('изменим второе число в масиве на масив ' + arr[2])
+ 
+   console.log('посмотрим весь масив' + arr)
+   arr.push(23)
+   console.log('добавим к масиву число 23 ')
+   console.log('посмотрим на масив ещё раз ' + arr)
+   arr.pop()
+   console.log('уберём последнее число и посмотрим на массив ' + arr)
+  arr.shift()
+   console.log(arr)
+   arr.unshift(3)
+   console.log(arr)
+
+
+}
+
+
