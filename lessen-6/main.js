0c154e9a5bd0b28e6f95db29f38f4c39cc452c63
russiankvas <59476082+russiankvas@users.noreply.github.com>
2020-01-06 13:15:29 +0400

    Add files via upload

---
+++ lessen-6/main.js
@@ -0,0 +1,63 @@
+﻿function main(){
+  let tree = {
+    data: 'A',
+    L: {
+      data: 'B',
+     L: 0,
+     R: 0
+    },
+      R: {
+      data: 'C',
+      L: 0,
+      R: 0
+    },
+  }
+  count(tree)
+  console.log(count(tree))
+  console.log(tree.__proto__)
+}
+
+//function count(tree) {
+  if (tree === 0) {
+    return 0;
+  } else {
+    return (1+ count(tree.L) + count (tree.R));
+  }; 
+}
+
+function add_node(tree, node) {
+  if (tree === 0) {
+    tree = node;
+    return;
+  }  
+
+  tree.count += 1;
+
+  if (tree.L === 0) {
+    tree.L = node;
+    return;
+  }
+
+  if (tree.R === 0) {
+    tree.R = node;
+    return;
+  }
+
+  if (tree.L.count < tree.R.count) {
+    add_node(tree.L, node);
+  } else {
+    add_node(tree.R, node);
+  
+  }
+}
+
+function create_node(data)
+  {return
+    {data,
+    count: 0
+    L: 0
+    R: 0 
+  }
+}
+
+  
