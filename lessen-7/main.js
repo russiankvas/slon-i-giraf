ef4a83c7516a6cbfd01230c0a08ba54e056dc2ad
russiankvas <59476082+russiankvas@users.noreply.github.com>
2020-01-06 18:37:53 +0400

    Add files via upload

---
+++ lessen-7/main.js
@@ -0,0 +1,31 @@
+﻿function main() {
+    let list = create(2007);
+    list.hash = calc_hash(list.data, list.data % 13);
+    
+    let elem = create(208);
+    list = add_elem(list, elem);
+
+    elem = create(105);
+    list = add_elem(list, elem);
+    
+    console.log(list); 
+}
+
+function create(data){
+     return {
+      data,
+      hash: 0,
+      prev: 0
+    }
+}
+
+function add_elem(list, elem) {
+   elem.hash = calc_hash(list.hash, elem.data);
+   elem.prev = list;
+   return elem;
+   
+}
+
+function calc_hash(n_1, n_2) {
+  return (1 + n_1*n_2) % (1 + n_1 + n_2);
+}
