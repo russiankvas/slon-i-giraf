534c204e00cff8c04b962533730f02989f1dc35f
russiankvas <59476082+russiankvas@users.noreply.github.com>
2020-01-07 12:15:19 +0400

    Update main.js

--- lessen-7/main.js
+++ lessen-7/main.js
@@ -1,29 +1,34 @@
-﻿function main() {
-    let list = create(2007);
-    list.hash = calc_hash(list.data, list.data % 13);
+function main() {
+    let list = create('slon', 'giraf', 6);
     
-    let elem = create(208);
+    let elem = create('Bora', 'Mikhael', 30000);
     list = add_elem(list, elem);
 
-    elem = create(105);
+    elem = create('Dasha', 'Vadim', 100);
     list = add_elem(list, elem);
-    
+
     console.log(list); 
 }
 
-function create(data){
+function create(from, to, amount) {
      return {
-      data,
+      data: {
+	from,	
+	to,
+	amount
+      },
       hash: 0,
       prev: 0
     }
 }
 
 function add_elem(list, elem) {
-   elem.hash = calc_hash(list.hash, elem.data);
+   let a = parseInt(elem.data.from, 36);
+   let b = parseInt(elem.data.to, 36);
+   let n = (a + b + elem.data.amount)
+   elem.hash = calc_hash(list.hash, n);
    elem.prev = list;
    return elem;
-   
 }
 
 function calc_hash(n_1, n_2) {
